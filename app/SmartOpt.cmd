46d8e3acb0940ce6ab3c145863cfc08ef9766a92
Elvis Abene <73388011+elvisabene@users.noreply.github.com>
2023-03-01 20:31:37 +0300

    added app files

---
+++ app/SmartOpt.cmd
@@ -0,0 +1,2 @@
+@echo off 
+start ./binaries/SmartOpt.exe 
--- utils/publish.cmd
+++ utils/publish.cmd
@@ -5,10 +5,13 @@ set directory=..\publish\
 msbuild /t:Clean,Build ^
         /p:Configuration=Release ^
         /p:TargetFrameworkVersion=v3.5 ^
-        /p:OutputPath="..\..\..\publish\bin" ^
+        /p:OutputPath="..\..\..\app\binaries" ^
         /p:PublishProfile=FolderProfile ^
         /p:SkipInvalidConfigurations=true ^
         /p:PlatformTarget=x86 ^
 ..\src\SmartOpt\SmartOpt\ >> nul
 
-rmdir /s /q ..\publish\binapp.publish
+rmdir /s /q ..\app\binariesapp.publish
+
+echo @echo off > ..\app\SmartOpt.cmd
+echo start ./binaries/SmartOpt.exe >> ..\app\SmartOpt.cmd
